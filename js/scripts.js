8b018360c431643609380fe771ac74c6d6cbffa9
Jo <joannadawnmiller@gmail.com>
2021-01-27 19:50:40 -0500

    add listener to ROLL buttons and run prototypes on clicks
    Co-Authored By: Michael Burton <micbur1@gmail.com>
    Co-Authored By: Maxwell Meyer <maxreadswell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,16 @@ this.turnScore = turnScore;
 this.totalScore = totalScore;
 this.status = status;
 }
-
 let player1 = new Player(1, [], 0, false);
 let player2 = new Player(2, [], 0, false);
-
-
 // generate random "dice roll" number from 1-6
 
 let rollsArray = [];
-
 Player.prototype.rollDice = function() {
   let rollValue = Math.floor(Math.random() * 6) + 1
   console.log(rollValue);
   rollsArray.push(rollValue);
 };
-
 Player.prototype.endTurn = function() {
   let sum = 0;
   if (rollsArray.includes(1)) {
@@ -30,25 +25,26 @@ Player.prototype.endTurn = function() {
   this.turnScore.push(sum);
   rollsArray = [];
 };
-
-
-
 // user logic//
 $(document).ready(function(){
-
   $("#play").click(function(){
     $(".player1").show();
     $("#play").hide();
   });
-
   $("#pass-player1").click(function(){
-    
+    player1.endTurn();
     $(".player2").show();
     $(".player1").slideUp();
     });
-
   $("#pass-player2").click(function(){
+    player2.endTurn();
     $(".player1").show();
-    $(".player2").slideUp();
+    $(".player2").slideUp();  
+  });
+  $("#roll-player1").click(function(){
+    player1.rollDice();
+  });
+  $("#roll-player2").click(function(){
+    player2.rollDice();
   });
 });
